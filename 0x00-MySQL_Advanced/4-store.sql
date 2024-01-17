5c6702f95a9364d5f3c832f278fd9afaceccbfc3
bamansour1978 <karatman@gmail.com>
2024-01-17 23:57:29 +0100

    Up

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,13 +1,13 @@
 -- Task 4
 
 DROP TRIGGER IF EXISTS reduce_quantity;
-DELIMIER $$
-CREATE TIGGER reduce_quantity
+DELIMITER $$
+CREATE TRIGGER reduce_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-	UPDATE items
-		SET quantity = quantity - NEW.number
-		WHERE name = NEW.item_name;
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
 END $$
-DELIMITER
+DELIMITER ;
