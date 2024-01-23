4d4a012339ba06d90efbcd6175fb270204ae67ce
bamansour1978 <karatman@gmail.com>
2024-01-23 10:48:31 -0800

    14. Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module Documentation
+"""
+
+
+def top_students(mongo_collection):
+    """ Function Documentation"""
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
