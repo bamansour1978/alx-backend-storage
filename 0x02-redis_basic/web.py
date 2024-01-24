2fc59100ee6c84795c3bbcf9c571d76021650768
bamansour1978 <karatman@gmail.com>
2024-01-24 15:48:58 +0100

    Up

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,25 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""
+Module documentation
+"""
 import redis
 import requests
 from datetime import timedelta
 
 
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
     '''
-    if not url or not url.strip():
+    Function
+    '''
+    if url is None or len(url.strip()) == 0:
         return ''
-
     redis_store = redis.Redis()
-    res_key = f'result:{url}'
-    req_key = f'count:{url}'
-
-    try:
-        result = redis_store.get(res_key)
-        if result is not None:
-            redis_store.incr(req_key)
-            return result.decode('utf-8')
-
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        result = response.content.decode('utf-8')
-
-        redis_store.setex(res_key, timedelta(seconds=10), result)
+    res_key = 'result:{}'.format(url)
+    req_key = 'count:{}'.format(url)
+    result = redis_store.get(res_key)
+    if result is not None:
         redis_store.incr(req_key)
         return result
-
-    except requests.RequestException as e:
-        print(f"Error making request to {url}: {e}")
-        return ''
+    result = requests.get(url).content.decode('utf-8')
+    redis_store.setex(res_key, timedelta(seconds=10), result)
+    return result
