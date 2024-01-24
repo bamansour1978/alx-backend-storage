6628d50eae8886d70efa193943e3080c5c62da86
bamansour1978 <karatman@gmail.com>
2024-01-24 15:41:02 +0100

    Up

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,18 +8,18 @@ from datetime import timedelta
 
 
 def get_page(url: str) -> str:
-    """
+    '''
     Function documentation
-    """
+    '''
     if url is None or len(url.strip()) == 0:
-        return ""
+        return ''
     redis_store = redis.Redis()
     res_key = 'result:{}'.format(url)
     req_key = 'count:{}'.format(url)
-    reslt = redis_store.get(res_key)
-    if reslt is not None:
+    result = redis_store.get(res_key)
+    if result is not None:
         redis_store.incr(req_key)
-        return reslt
-    reslt = requests.get(url).content.decode('utf-8')
-    redis_store.setex(res_key, timedelta(seconds=10), reslt)
-    return reslt
+        return result
+    result = requests.get(url).content.decode('utf-8')
+    redis_store.setex(res_key, timedelta(seconds=10), result)
+    return result
